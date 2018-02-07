e138ea9eeb191fd76ac58a2e524501de19a10ff1
benpak2015 <benpak2015@gmail.com>
2018-02-07 21:01:49 +0900

    initial commit

--- mnist_cnn_layers.py
+++ mnist_cnn_layers.py
@@ -20,3 +20,17 @@ class Model:
         with tf.variable_scope(self.name):
             self.training = tf.placehloder(tf.bool)
             self.X = tf.placeholder(tf.float32, [None, 784])
+            X_img = tf.reshape(self.X, [-1, 28, 28, 1])
+            self.Y = tf.placeholder(tf.float32, [None, 10])
+
+            conv1 = tf.layers.conv2d(inputs=X_img, filters=32, kernel_size=[3,3], padding="SAME", activation=tf.nn.relu)
+            pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2,2], padding="SAME", strides=2)
+            dropout1 = tf.layers.dropout(inputs=pool1, rate=0.7, training=self.training)
+
+            conv2 = tf.layers.conv2d(inputs=dropout1, filters=64, kernel_size=[3,3])
+            pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2,2], padding="SAME", strides=2)
+            dropout2= tf.layers.dropout(inputs=pool2, rate=0.7, training=self.training)
+
+            conv3 = tf.layers.conv2d(inputs=dropout2, filters=128, kernel_size=[3,3], padding="SAME", activation=tf.nn.relu)
+            pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2,2], padding="SAME", strides=2)
+            dropout3 = tf.layers.drop
