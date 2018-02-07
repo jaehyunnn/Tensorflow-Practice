175dbff53406dbdb7025f78576894e144296e253
benpak2015 <benpak2015@gmail.com>
2018-02-07 20:40:03 +0900

    initial commit

---
+++ mnist_cnn_layers.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+tf.set_random_seed(777)
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+learning_rate = 0.001
+training_epochs = 15
+batch_size = 100
+
+class Model:
+    def __init__(self, sess, name):
+        self.sess = sess
+        self.name = name
+        self._build_net()
+
+    def _build_net(self):
+        with tf.variable_scope(self.name):
+            self.training = tf.placehloder(tf.bool)
+            self.X = tf.placeholder(tf.float32, [None, 784])
