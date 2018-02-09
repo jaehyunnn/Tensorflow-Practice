ca2a51d2a46d7aad6b640c8c878f88c98100ce67
benpak2015 <benpak2015@gmail.com>
2018-02-09 17:03:01 +0900

    initial commit

--- mnist_cnn_layers.py
+++ mnist_cnn_layers.py
@@ -27,9 +27,9 @@ class Model:
             pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2,2], padding="SAME", strides=2)
             dropout1 = tf.layers.dropout(inputs=pool1, rate=0.7, training=self.training)
 
-            conv2 = tf.layers.conv2d(inputs=dropout1, filters=64, kernel_size=[3,3])
+            conv2 = tf.layers.conv2d(inputs=dropout1, filters=64, kernel_size=[3,3], padding="SAME", activation=tf.nn.relu)
             pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2,2], padding="SAME", strides=2)
-            dropout2= tf.layers.dropout(inputs=pool2, rate=0.7, training=self.training)
+            dropout2 = tf.layers.dropout(inputs=pool2, rate=0.7, training=self.training)
 
             conv3 = tf.layers.conv2d(inputs=dropout2, filters=128, kernel_size=[3,3], padding="SAME", activation=tf.nn.relu)
             pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2,2], padding="SAME", strides=2)
@@ -37,32 +37,42 @@ class Model:
 
             flat = tf.reshape(dropout3, [-1, 128*4*4])
             dense4 = tf.layers.dense(flat, units=625, activation=tf.nn.relu)
-            dropout4 = tf.layers.dropout(dense4, rate=0.7,training=self.training)
+            dropout4 = tf.layers.dropout(dense4, rate=0.5, training=self.training)
 
             self.logits = tf.layers.dense(inputs=dropout4, units=10)
 
         self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.Y))
         self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.cost)
 
-        correct_prediction = tf.equal(tf.argmax(self.logits,1),tf.argmax(self.Y,1))
+        correct_prediction = tf.equal(tf.argmax(self.logits, 1), tf.argmax(self.Y, 1))
         self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    def predict(self, x_text, training=False):
+    def predict(self, x_test, training=False):
         return self.sess.run(self.logits, feed_dict={self.X: x_test, self.training: training})
 
     def get_accuracy(self, x_test, y_test, training=False):
-        return self.sess.run(self.accuracy, feed_dict={X: x_test, self.Y: y_test, self.training: training})
+        return self.sess.run(self.accuracy, feed_dict={self.X: x_test, self.Y: y_test, self.training: training})
 
     def train(self, x_data, y_data, training=True):
-        return self.sess.run([self.cost, self.optimizer], feed_dict={self.X:x_data, self.Y:y_data, self.training:training})
+        return self.sess.run([self.cost, self.optimizer],
+                             feed_dict={self.X: x_data, self.Y: y_data, self.training: training})
 
-sess=tf.Session()
+sess = tf.Session()
 m1 = Model(sess, "m1")
 
 sess.run(tf.global_variables_initializer())
 
 print("Learning Started!")
-
 for epoch in range(training_epochs):
     avg_cost = 0
-    total
+    total_batch = int(mnist.train.num_examples / batch_size)
+
+    for i in range(total_batch):
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        c, _ = m1.train(batch_xs, batch_ys)
+        avg_cost += c/total_batch
+
+    print('Epoch:', '%04d' % (epoch+1), 'cost=', '{:.9f}'.format(avg_cost))
+print("Learning Finished!")
+
+print('Accuracy:',m1.get_accuracy(mnist.test.images, mnist.test.labels))
