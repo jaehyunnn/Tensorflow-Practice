6db025551f7ceb6cc548eb7e989eb81136260231
benpak2015 <benpak2015@gmail.com>
2018-02-09 09:55:39 +0900

    update

--- mnist_cnn_layers.py
+++ mnist_cnn_layers.py
@@ -18,7 +18,7 @@ class Model:
 
     def _build_net(self):
         with tf.variable_scope(self.name):
-            self.training = tf.placehloder(tf.bool)
+            self.training = tf.placeholder(tf.bool)
             self.X = tf.placeholder(tf.float32, [None, 784])
             X_img = tf.reshape(self.X, [-1, 28, 28, 1])
             self.Y = tf.placeholder(tf.float32, [None, 10])
@@ -33,4 +33,36 @@ class Model:
 
             conv3 = tf.layers.conv2d(inputs=dropout2, filters=128, kernel_size=[3,3], padding="SAME", activation=tf.nn.relu)
             pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2,2], padding="SAME", strides=2)
-            dropout3 = tf.layers.drop
+            dropout3 = tf.layers.dropout(inputs=pool3, rate=0.7, training=self.training)
+
+            flat = tf.reshape(dropout3, [-1, 128*4*4])
+            dense4 = tf.layers.dense(flat, units=625, activation=tf.nn.relu)
+            dropout4 = tf.layers.dropout(dense4, rate=0.7,training=self.training)
+
+            self.logits = tf.layers.dense(inputs=dropout4, units=10)
+
+        self.cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.Y))
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.cost)
+
+        correct_prediction = tf.equal(tf.argmax(self.logits,1),tf.argmax(self.Y,1))
+        self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    def predict(self, x_text, training=False):
+        return self.sess.run(self.logits, feed_dict={self.X: x_test, self.training: training})
+
+    def get_accuracy(self, x_test, y_test, training=False):
+        return self.sess.run(self.accuracy, feed_dict={X: x_test, self.Y: y_test, self.training: training})
+
+    def train(self, x_data, y_data, training=True):
+        return self.sess.run([self.cost, self.optimizer], feed_dict={self.X:x_data, self.Y:y_data, self.training:training})
+
+sess=tf.Session()
+m1 = Model(sess, "m1")
+
+sess.run(tf.global_variables_initializer())
+
+print("Learning Started!")
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+    total
